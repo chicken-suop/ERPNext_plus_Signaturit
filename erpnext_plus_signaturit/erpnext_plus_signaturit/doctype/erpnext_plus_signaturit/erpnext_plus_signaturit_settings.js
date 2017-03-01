3e6914875ce79ad6603cd1c786241735c4c70c17
_ratskin_ <elliot.schep@gmail.com>
2017-03-01 10:09:48 +0100

    Removed doctype dir

--- erpnext_plus_signaturit/erpnext_plus_signaturit/doctype/erpnext_plus_signaturit/erpnext_plus_signaturit_settings.js
+++
@@ -1,9 +0,0 @@
-frappe.ui.form.on("Amazon S3 Settings", {
-    onload: function (frm) {
-        if (typeof frm.doc.company_name === 'undefined') {
-            cur_frm.set_value("company_name", frappe.defaults.get_user_default("Company"));
-
-        };
-    }
-});
-
